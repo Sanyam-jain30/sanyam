1dfd37d53cafe882b7255765dcbc75018fd0b300
Sanyam Jain <sanyamjain3003@gmail.com>
2024-02-16 21:30:05 +0530

    Version: 9.4 - Added Filter Option

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-const researchAddOn = document.getElementsByClassName("more")[0];
+// const researchAddOn = document.getElementsByClassName("more")[0];
 
-researchAddOn.addEventListener('click', () => {
-    var researchCards = document.getElementsByClassName("research-card");
-    for(let card of researchCards){
-        if(card.style.display == "none"){
-            card.style.display = "flex";
-        }
+// researchAddOn.addEventListener('click', () => {
+//     var researchCards = document.getElementsByClassName("research-card");
+//     for(let card of researchCards){
+//         if(card.style.display == "none"){
+//             card.style.display = "flex";
+//         }
+//     }
+
+//     researchAddOn.style.display = "none";
+// });
+
+function handleLabelClick(id, num, component, card, section) {
+    console.log(id, num, component, card);
+    var tab = document.getElementById(id);
+    var glider = document.querySelector(`#${component} .glider`);
+    var projectList = document.querySelectorAll(`#${component} .${card}`);
+    console.log(glider, projectList);
+
+    glider.style.transform = 'translateX(' + 230 * num + 'px)';
+    console.log(id, id==="all");
+
+    if(id == "all"){
+        console.log("yes");
+        projectList.forEach(function(project) {
+            project.style.display = '';
+        });
+    } else {
+        projectList.forEach(function(project) {
+            var decision = project.querySelector(`.${section}`);
+
+            if (decision && decision.classList.contains(id)) {
+                project.style.display = '';
+            } else {
+                project.style.display = 'none';
+            }
+        });
     }
-
-    researchAddOn.style.display = "none";
-});
+}
